c69f82d2a4485cb6eebeabe964145e15233bc7d3
faiiidy <fiadyrhaiem2@gmail.com>
2025-12-22 16:22:00 +0100

    Detecters Done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,85 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]private float moveSpeed = 7.0f;
-    private void Update()
+    [SerializeField]private GameInput gameInput;
+    [SerializeField]private LayerMask countersLayerMask;
+    private bool isMoving = false;
+    private Vector3 lastInteractDirc;
+    private void Start()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if( Input.GetKey(KeyCode.W) ) {inputVector.y = 1; }
-        if ( Input.GetKey(KeyCode.S) ) { inputVector.y = -1; }
-        if ( Input.GetKey(KeyCode.D) ) { inputVector.x = 1; }
-        if ( Input.GetKey(KeyCode.A) ) { inputVector.x = -1; }
-        inputVector = inputVector.normalized;
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
-        Debug.Log(inputVector);
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDirc = moveDir;
+        }
+        float interactionDistance = 2.0f;
+        if (Physics.Raycast(transform.position, lastInteractDirc, out RaycastHit raycastHit, interactionDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+
+    }
+
+    private void Update()
+    {       
+        HandleMovement();
+        HandleInteraction();
+    }
+    public bool IsMoving()
+    {
+               return isMoving;
+    }
+    private void HandleInteraction()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDirc = moveDir;
+        }
+
+    }
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRaduis = 0.7f;
+        float playerHeight = 2.0f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.up * playerHeight, playerRaduis, moveDir, moveDistance);
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.up * playerHeight, playerRaduis, moveDirX, moveDistance);
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.up * playerHeight, playerRaduis, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        isMoving = moveDir != Vector3.zero;
+        float rotationSpeed = 10.0f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
     }
 }
