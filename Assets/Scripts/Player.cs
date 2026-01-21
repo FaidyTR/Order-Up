86538dcf0506119359bd1acab07cae8424a1e50e
faiiidy <faidyrhaiem2@gmail.com>
2026-01-21 12:51:10 +0100

    problem in detection solved

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,115 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class Player : MonoBehaviour
 {
-    public static Player Instance { get; private set; }
-
-    public event EventHandler <OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
-    public class OnSelectedCounterChangedEventArgs : EventArgs
-    {
-        public ClearCounter selectedCounter;
-    }
-
-
-    [SerializeField]private float moveSpeed = 7.0f;
-    [SerializeField]private GameInput gameInput;
-    [SerializeField]private LayerMask countersLayerMask;
-    private bool isMoving = false;
-    private Vector3 lastInteractDirc;
-    private ClearCounter selectedCounter;
-
-    private void Awake()
-    {
-        if (Instance != null)
-        {
-            Debug.LogError("There is more than one Player instance");
-        }
-        Instance = this;
-    }
-    private void Start()
-    {
-        gameInput.OnInteractAction += GameInput_OnInteractAction;
-    }
-
-    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
-    {
-    if (selectedCounter != null)
-        {
-            selectedCounter.Interact();
-        }
-    }
-
+    [SerializeField] private float PlayerMoveSpeed = 7f;
+    private bool isWalking;
     private void Update()
-    {       
-        HandleMovement();
-        HandleInteraction();
-    }
-    public bool IsMoving()
     {
-               return isMoving;
+        MovePlayer();
     }
-    private void HandleInteraction()
+    private void MovePlayer()
     {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        if (moveDir != Vector3.zero)
-        {
-            lastInteractDirc = moveDir;
-        }
-        float interactionDistance = 2.0f;
-        if (Physics.Raycast(transform.position, lastInteractDirc, out RaycastHit raycastHit, interactionDistance, countersLayerMask))
-        {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
-            {
-                SetSlectedCounter(clearCounter);
-            }
-            else { SetSlectedCounter(null); }
-        }
-        else { SetSlectedCounter(null); }
+        Vector2 inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W)) { inputVector.y += 1; }
+        if (Input.GetKey(KeyCode.S)) { inputVector.y -= 1; }
+        if (Input.GetKey(KeyCode.D)) { inputVector.x += 1; }
+        if (Input.GetKey(KeyCode.A)) { inputVector.x -= 1; }
+        Vector3 directionVector = new Vector3(inputVector.x, 0, inputVector.y).normalized;
+        transform.position += directionVector * Time.deltaTime * PlayerMoveSpeed;
+        isWalking = directionVector != Vector3.zero;
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, directionVector, Time.deltaTime * rotateSpeed);
     }
-    private void HandleMovement()
+    public bool IsWalking()
     {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        float moveDistance = moveSpeed * Time.deltaTime;
-        float playerRaduis = 0.7f;
-        float playerHeight = 2.0f;
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.up * playerHeight, playerRaduis, moveDir, moveDistance);
-        if (!canMove)
-        {
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
-            canMove = !Physics.CapsuleCast(transform.position, transform.up * playerHeight, playerRaduis, moveDirX, moveDistance);
-            if (canMove)
-            {
-                moveDir = moveDirX;
-            }
-            else
-            {
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
-                canMove = !Physics.CapsuleCast(transform.position, transform.up * playerHeight, playerRaduis, moveDirZ, moveDistance);
-                if (canMove)
-                {
-                    moveDir = moveDirZ;
-                }
-            }
-        }
-        if (canMove)
-        {
-            transform.position += moveDir * moveDistance;
-        }
-        isMoving = moveDir != Vector3.zero;
-        float rotationSpeed = 10.0f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
-    }
-    private void SetSlectedCounter(ClearCounter selectedCounter)
-    {
-        this.selectedCounter = selectedCounter;
-        if (OnSelectedCounterChanged != null)
-        {
-            OnSelectedCounterChanged(this, new OnSelectedCounterChangedEventArgs { selectedCounter = selectedCounter });
-        }
+        return isWalking;
+
     }
 }
+
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private const string Is_Walking = "IsWalking";
+    [SerializeField] private Player player;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        animator.SetBool(Is_Walking, player.IsWalking());
+    }
+
+
+}
