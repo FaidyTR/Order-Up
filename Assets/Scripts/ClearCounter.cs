3c531ed908b9bdd5a01d42c363b5553ba8cd9c50
faiiidy <faidyrhaiem2@gmail.com>
2026-01-25 00:08:04 +0100

    SciptableObjects Created

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-    [SerializeField] private Transform TomatoPrefab;
+    [SerializeField] private Transform Prefab;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform TopPointSpawn;
 
     public void Interact()
     {
         Debug.Log("Interacted with " + gameObject.transform);
-        Transform tomaotoSpawned = Instantiate(TomatoPrefab, TopPointSpawn);
-        tomaotoSpawned.localPosition = Vector3.zero;
-        //hello 
+        Transform kitchenObjectSpawned = Instantiate(Prefab, TopPointSpawn);
+        kitchenObjectSpawned.localPosition = Vector3.zero;
+        Debug.Log("Spawned " + kitchenObjectSpawned.GetComponent<KitchenObject>().GetKitchenObjectSO().ObjectName);
     }
 }
---
+++ Assets/Scripts/KitchenObjectSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[CreateAssetMenu()]
+public class KitchenObjectSO : ScriptableObject
+{
+    public string ObjectName;
+    public Transform Prefab;
+    public Sprite Sprite;
+
+}
