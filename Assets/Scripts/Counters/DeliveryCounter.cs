d2c3f58bfeb64ebeb5994880ee8232d789d08c0b
FaidyTR <faidyrhaiem2@gmail.com>
2026-03-27 17:38:51 +0100

    DeliveredManager Visual Completed

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(IKitchenObjectParent player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                plateKitchenObject.DestroySelf();
+            }
+        }
+    }
+}
