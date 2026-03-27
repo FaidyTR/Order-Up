3bf6052128f89683106111843f9c0a64539bcfd3
FaidyTR <faidyrhaiem2@gmail.com>
2026-03-27 19:50:28 +0100

    ReciepesDelivered

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -9,6 +9,7 @@ public class DeliveryCounter : BaseCounter
             if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
             {
                 plateKitchenObject.DestroySelf();
+                RecipiesManager.Instance.DeliverRecipie(plateKitchenObject);
             }
         }
     }
--- Assets/Scripts/RecipiesManager.cs
+++ Assets/Scripts/RecipiesManager.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 using System.Collections.Generic;
 public class RecipiesManager : MonoBehaviour
 {
+    public static RecipiesManager Instance { get; private set; }
+
     [SerializeField] private RecipiesListSO RecipiesListSO;
     private List<RecipieSO> waitingRecipies;
     private float timer;
     private float waitingTimer = 2f;
     private int maxWaitingRecipies = 4;
 
+    private void Awake()
+    {
+        Instance = this;
+    }
     private void Start()
     {
         waitingRecipies = new List<RecipieSO>();
@@ -27,6 +33,40 @@ public class RecipiesManager : MonoBehaviour
             }
         }
 
+    }
+    public void DeliverRecipie(PlateKitchenObject plateKitchenObject)
+    {
+        for (int i = 0; i < waitingRecipies.Count; i++)
+        {
+            if(PlateKitchenObjectMatchWaitingRecipie(plateKitchenObject, waitingRecipies[i]))
+            {
+                Debug.Log("Recipie Delivered! " + waitingRecipies[i].Name);
+                waitingRecipies.RemoveAt(i);
+                return;
+            }
+        }
+            Debug.Log("Recipie Wrong!");
+    }
+    private bool PlateKitchenObjectMatchWaitingRecipie(PlateKitchenObject plateKitchenObject, RecipieSO recipieSO)
+    {
+        var plateList = plateKitchenObject.GetKitchenObjectSOList();
+        var recipieList = recipieSO.kitchenObjectSOList;
+        if (plateList == null || recipieList == null) return false;
+        if (plateList.Count != recipieList.Count) return false;
+
+
+        List<KitchenObjectSO> recipieCopy = new List<KitchenObjectSO>(recipieList);
+
+        foreach (KitchenObjectSO plateObj in plateList)
+        {
+            if (recipieCopy.Contains(plateObj))
+            {
+                recipieCopy.Remove(plateObj);
+            }
+            else return false;
+        }
+
+        return recipieCopy.Count == 0;
     }
 
 }
