9c864705218b0cb86b3d7060ae199ba6e63d1c2f
FaidyTR <faidyrhaiem2@gmail.com>
2026-04-05 13:35:59 +0100

    Updates: UI improvements

--- Assets/Scripts/UI/GamePlayingUI.cs
+++ Assets/Scripts/UI/GamePlayingUI.cs
@@ -9,12 +9,12 @@ public class GamePlayingUI : MonoBehaviour
 
     private void Awake()
     {
-        Hide();
     }
     private void Start()
     {
         GameManager.Instance.OnStateChanged += GameManager_OnStateChanged;
         timerCount.fillAmount = 0f;
+        Hide();
     }
     private void GameManager_OnStateChanged(object sender, System.EventArgs e)
     {
@@ -23,17 +23,10 @@ public class GamePlayingUI : MonoBehaviour
             Show();
         }
     }
-    private void Update()
-    {
-        if (!GameManager.Instance.GetStateIsGamePlaying())
-        {
-            Hide();
-        }
-    }
     private void Hide()
     {
-        backGround.gameObject.SetActive(false);
         timerCount.gameObject.SetActive(false);
+        backGround.gameObject.SetActive(true);
     }
     private void Show()
     {
